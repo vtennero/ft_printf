0b442e578669dddc34bc8bc267206c5a312e8e28
Victor TENNERONI <vtennero@student.42.fr>
2017-12-14 12:55:54 +0100

    Tracer Tong

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: vtennero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/07 13:46:36 by vtennero          #+#    #+#             */
-/*   Updated: 2017/12/13 21:38:41 by vtennero         ###   ########.fr       */
+/*   Updated: 2017/12/14 12:55:43 by vtennero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,17 @@ enum
 	HASH, PLUS, MINUS, SPACE, ZERO, H, L, J, Z, HH, LL
 };
 
+enum
+{
+	STR, L_STR, PTR_A, DEC, L_S_DEC, INTGR, U_OCT, L_U_OCT, U_DEC, L_U_DEC, 
+	U_HEXA, L_U_HEXA, CHR, L_CHR
+};
+
 typedef struct	s_params
 {
 	int			width;
 	int			prec;
-	int			spec;
+	t_bool		spec[L_CHR + 1];
 	t_bool		flags[LL + 1];
 }				t_params;
 
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: vtennero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/07 11:52:18 by vtennero          #+#    #+#             */
-/*   Updated: 2017/12/13 23:22:12 by vtennero         ###   ########.fr       */
+/*   Updated: 2017/12/14 12:55:40 by vtennero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,17 @@ int		ft_set_flags(t_params *arg, char *str, int *index)
 
 int		ft_set_length(t_params *arg, char *str, int *index)
 {
-	if (ft_is_char(str[*index], 'h') == 1)
+	if (ft_strnequ(str + *index, "hh", 2) == 1)
+	{
+		*index += 1;
+		return (arg->flags[HH] = 1);
+	}
+	if (ft_strnequ(str + *index, "ll", 2) == 1)
+	{
+		*index += 1;
+		return (arg->flags[LL] = 1);
+	}	
+	else if (ft_is_char(str[*index], 'h') == 1)
 		return(arg->flags[H] = 1);	
 	else if (ft_is_char(str[*index], 'l') == 1)
 		return(arg->flags[L] = 1);	
@@ -54,14 +64,70 @@ void		ft_set_bool_flags(t_params *arg, int n)
 	arg->flags[n] = 1;	
 }
 
-void		ft_set_width(t_params *arg, int n)
+int		ft_set_width(t_params *arg, char *str, int *index)
 {
-	arg->width = n;
+	if (ft_isdigit((int)str[*index]) == 1)
+	{
+		arg->width = 0;
+		while (ft_isdigit((int)str[*index]) == 1)
+		{
+			arg->width = 10 * arg->width + (int)str[*index] - 48;
+			*index += 1;
+		}
+		*index -= 1;
+		return (1);
+	}
+	return (0);
 }
 
-void		ft_set_prec(t_params *arg, int n)
+int		ft_set_prec(t_params *arg, char *str, int *index)
 {
-	arg->prec = n;
+	if (ft_is_char(str[*index], '.') == 1)
+	{
+		*index += 1;
+		arg->prec = 0;
+		while (ft_isdigit((int)str[*index]) == 1)
+		{
+			arg->prec = 10 * arg->prec + (int)str[*index] - 48;
+			*index += 1;
+		}
+		*index -= 1;
+		return (1);
+	}
+	return (0);
+}
+
+int		ft_set_spec(t_params *arg, char *str, int *index)
+{
+	if (ft_is_char(str[*index], 's') == 1)
+		return (arg->spec[STR] = 1);
+	else if (ft_is_char(str[*index], 'S') == 1)
+		return (arg->spec[L_STR] = 1);
+	else if (ft_is_char(str[*index], 'p') == 1)
+		return (arg->spec[PTR_A] = 1);
+	else if (ft_is_char(str[*index], 'd') == 1)
+		return (arg->spec[DEC] = 1);
+	else if (ft_is_char(str[*index], 'D') == 1)
+		return (arg->spec[L_S_DEC] = 1);
+	else if (ft_is_char(str[*index], 'i') == 1)
+		return 	(arg->spec[INTGR] = 1);
+	else if (ft_is_char(str[*index], 'o') == 1)
+		return (arg->spec[U_OCT] = 1);
+	else if (ft_is_char(str[*index], 'O') == 1)
+		return (arg->spec[L_U_OCT] = 1);
+	else if (ft_is_char(str[*index], 'u') == 1)
+		return (arg->spec[U_DEC] = 1);
+	else if (ft_is_char(str[*index], 'U') == 1)
+		return (arg->spec[L_U_DEC] = 1);
+	else if (ft_is_char(str[*index], 'x') == 1)
+		return (arg->spec[U_HEXA] = 1);
+	else if (ft_is_char(str[*index], 'X') == 1)
+		return (arg->spec[L_U_HEXA] = 1);
+	else if (ft_is_char(str[*index], 'x') == 1)
+		return (arg->spec[CHR] = 1);
+	else if (ft_is_char(str[*index], 'C') == 1)
+		return (arg->spec[L_CHR] = 1);
+	return (0);
 }
 
 t_params	*ft_create_params(void)
@@ -94,17 +160,33 @@ t_params	*ft_set_zero_params(t_params *arg)
 
 void	ft_print_params(t_params *arg)
 {
-	int	i;
-
-	i = 0;
+	printf("%c : %d\n", '#', arg->flags[0]);
+	printf("%c : %d\n", '+', arg->flags[1]);
+	printf("%c : %d\n", '-', arg->flags[2]);
+	printf("%c : %d\n", ' ', arg->flags[3]);
+	printf("%c : %d\n", '0', arg->flags[4]);
+	printf("%c : %d\n", 'h', arg->flags[5]);
+	printf("%c : %d\n", 'l', arg->flags[6]);
+	printf("%c : %d\n", 'j', arg->flags[7]);
+	printf("%c : %d\n", 'z', arg->flags[8]);
+	printf("%s : %d\n", "hh", arg->flags[9]);
+	printf("%s : %d\n", "ll", arg->flags[10]);
 	printf("width = %d\n", arg->width);
 	printf("precision = %d\n", arg->prec);
-	while (i < 11)
-	{
-		printf("flag %d : %d\n", i, arg->flags[i]);
-		i++;
-	}
-	printf("spec = %d\n", arg->spec);
+	printf("%c : %d\n", 's', arg->spec[0]);
+	printf("%c : %d\n", 'S', arg->spec[1]);
+	printf("%c : %d\n", 'p', arg->spec[2]);
+	printf("%c : %d\n", 'd', arg->spec[3]);
+	printf("%c : %d\n", 'D', arg->spec[4]);
+	printf("%c : %d\n", 'i', arg->spec[5]);
+	printf("%c : %d\n", 'o', arg->spec[6]);
+	printf("%c : %d\n", 'O', arg->spec[7]);
+	printf("%c : %d\n", 'u', arg->spec[8]);
+	printf("%c : %d\n", 'U', arg->spec[9]);
+	printf("%c : %d\n", 'x', arg->spec[10]);
+	printf("%c : %d\n", 'X', arg->spec[11]);
+	printf("%c : %d\n", 'x', arg->spec[12]);
+	printf("%c : %d\n", 'C', arg->spec[13]);
 }
 
 int		ft_read_string(char *str)
@@ -127,6 +209,12 @@ int		ft_read_string(char *str)
 						;
 					else if (ft_set_length(&arg, str, &index) == 1)
 						;
+					else if (ft_set_width(&arg, str, &index) == 1)
+						;
+					else if (ft_set_prec(&arg, str, &index) == 1)
+						;
+					else if (ft_set_spec(&arg, str, &index) == 1)
+						;
 					else
 						break;
 					index++;
@@ -146,7 +234,7 @@ int		main(int ac, char **av)
 	if (ac == 2)
 		;
 	ft_read_string(av[1]);
-	//printf("");
+	printf("%+c\n", 'a');
 	//ft_putnbr(printf(""));
 	//ft_putchar(10);
 	//printf("%Z546", 6);
